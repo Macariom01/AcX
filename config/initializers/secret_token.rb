634650c8b7a3f3be9ba99ca51f66adb3cbf0aaeb
student <student@academyx.com>
2014-09-27 10:52:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AcX::Application.config.secret_key_base = 'f65c4fa1d96fc636d2100291bc4199a1c152822485a4884e92850b00bf5650ce27f5e40b8f0ce1b1e92e055a2c76e0e73d11d1e6bbee13911b2858c4a3fc9b37'
